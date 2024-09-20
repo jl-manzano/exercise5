e554ed5c14dce2fd54d768048a9c91c196f02ef2
jl.manzano <jl.manzano@106-19.info2>
2024-09-20 14:33:40 +0200

    exercise5 finished

---
+++ exercise5/src/exercise5/exercise5.java
@@ -0,0 +1,9 @@
+package exercise5;
+
+public class exercise5 {
+	public static void main(String[] args) {
+		 System.out.println("Nombre: José Luis Manzano");
+         System.out.println("Dirección: Calle Segre, 1");
+         System.out.println("Teléfono: 653-61-50-31");
+	}
+}
